842ef089b3e0d9f44e0a80297e5f5f1d86f5dad6
Wastik <mdwastikansari521@gmail.com>
2025-05-12 16:52:27 +0530

    project setup

---
+++ android/app/src/main/kotlin/com/example/spinovo_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.spinovo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:spinovo_app/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spinovo',
+      theme: ThemeData(),
+      home: const HomeScreen(),
+    );
+  }
+}
